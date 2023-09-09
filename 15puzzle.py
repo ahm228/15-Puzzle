88c75b50f0531d448e588a04f1216847098501dd
ahm228 <94765616+ahm228@users.noreply.github.com>
2023-09-09 10:48:22 -0500

    Implemented camel case

--- 15puzzle.py
+++ 15puzzle.py
@@ -1,4 +1,4 @@
-def is_goal(board): #Define a function to check if a given board is in a goal state
+def isGoal(board): #Define a function to check if a given board is in a goal state
     N = len(board)  #Get the size of the board (N x N)
     n = 1   #Initialize a variable to keep track of the expected value
     for i in range(N):
@@ -9,56 +9,56 @@ def is_goal(board): #Define a function to check if a given board is in a goal st
             if n == N * N:
                 return True #If all values are in order, return True
 
-def manhattan_distance(board):  #Define a function to calculate the Manhattan distance heuristic for a given board
+def manhattanDistance(board):  #Define a function to calculate the Manhattan distance heuristic for a given board
     N = len(board)  # Get the size of the board (N x N)
     distance = 0    # Initialize the total Manhattan distance
     for i in range(N):
         for j in range(N):
             if board[i][j] == 0:
                 continue    #Skip the empty cell
-            target_val = board[i][j] - 1    #Calculate the target value for the cell
-            target_x, target_y = divmod(target_val, N)  #Calculate the target position
-            distance += abs(i - target_x) + abs(j - target_y)   #Calculate Manhattan distance
+            targetVal = board[i][j] - 1    #Calculate the target value for the cell
+            targetX, targetY = divmod(targetVal, N)  #Calculate the target position
+            distance += abs(i - targetX) + abs(j - targetY)   #Calculate Manhattan distance
     return distance
 
-def ida_star(board):    #Define the IDA* (Iterative Deepening A*) search algorithm
+def idaStar(board):    #Define the IDA* (Iterative Deepening A*) search algorithm
     N = len(board)  #Get the size of the board (N x N)
     path = []   #Initialize a list to store the solution path
     
     def dfs(board, g, bound, path): #Define a depth-first search function with a depth limit (bound)
-        h = manhattan_distance(board)   #Calculate the Manhattan distance heuristic
+        h = manhattanDistance(board)   #Calculate the Manhattan distance heuristic
         f = g + h   #Calculate the evaluation function value (f = g + h)
         if f > bound:
             return f    #If f is greater than the current bound, prune this branch
-        if is_goal(board):
+        if isGoal(board):
             return -1   #If the board is in the goal state, return -1 to signal success
-        min_bound = float('inf')    #Initialize the minimum bound for this branch
+        minBound = float('inf')    #Initialize the minimum bound for this branch
         
-        zero_x, zero_y = 0, 0   #Initialize the coordinates of the empty cell
+        zeroX, zeroY = 0, 0   #Initialize the coordinates of the empty cell
         for i in range(N):
             for j in range(N):
                 if board[i][j] == 0:
-                    zero_x, zero_y = i, j   #Find the coordinates of the empty cell
+                    zeroX, zeroY = i, j   #Find the coordinates of the empty cell
                     
         for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:   #Try moving the empty cell in four possible directions: right, down, left, up
-            new_x, new_y = zero_x + dx, zero_y + dy #Calculate the new position
-            if 0 <= new_x < N and 0 <= new_y < N:   #Swap the values between the empty cell and the neighboring cell
-                board[zero_x][zero_y], board[new_x][new_y] = board[new_x][new_y], board[zero_x][zero_y]
-                path.append((new_x, new_y)) #Add the move to the path
+            newX,  = zeroX + dx, zeroY + dy #Calculate the new position
+            if 0 <= newX < N and 0 <= newY < N:   #Swap the values between the empty cell and the neighboring cell
+                board[zeroX][zeroY], board[newX][newY] = board[newX][newY], board[zeroX][zeroY]
+                path.append((newX, newY)) #Add the move to the path
                 
                 t = dfs(board, g + 1, bound, path)  #Recursively explore this move with an increased depth (g + 1)
                 
                 if t == -1:
                     return -1   #If the goal is found in this branch, return -1
-                if t < min_bound:
-                    min_bound = t   #Update the minimum bound
+                if t < minBound:
+                    minBound = t   #Update the minimum bound
                 
                 path.pop()  #Remove the move from the path (backtrack)
-                board[zero_x][zero_y], board[new_x][new_y] = board[new_x][new_y], board[zero_x][zero_y] #Undo the move
+                board[zeroX][zeroY], board[newX][newY] = board[newX][newY], board[zeroX][zeroY] #Undo the move
         
-        return min_bound    #Return the minimum bound for this branch
+        return minBound    #Return the minimum bound for this branch
 
-    bound = manhattan_distance(board)   #Initialize the bound with the Manhattan distance heuristic
+    bound = manhattanDistance(board)   #Initialize the bound with the Manhattan distance heuristic
     while True:
         t = dfs(board, 0, bound, path)  #Perform iterative deepening search
         if t == -1:
@@ -74,10 +74,10 @@ if __name__ == "__main__":  #Main program entry point
         [13, 14, 15, 12]
     ]
     
-    solution_path = ida_star(board) #Run the IDA* algorithm to find a solution path
-    if solution_path:   #Display the result
+    solutionPath = idaStar(board) #Run the IDA* algorithm to find a solution path
+    if solutionPath:   #Display the result
         print("Solution found:")
-        for move in solution_path:
+        for move in solutionPath:
             print(move) #Print each move in the solution path
     else:
         print("No solution found")  #Print a message if no solution is found
