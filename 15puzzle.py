96924b6909a6bbbffd2ea24c6567ebcd3bdf41d2
ahm228 <94765616+ahm228@users.noreply.github.com>
2023-09-09 11:15:03 -0500

    Optimization
    -corrected isGoal() function.
    -created a  visited set to remember states visited within each search depth, clearing it between different depths.

--- 15puzzle.py
+++ 15puzzle.py
@@ -1,100 +1,110 @@
 import random
 
 def generateRandomBoard(N):
-    board = [[0] * N for _ in range(N)] #Create an empty N x N game board filled with zeros
-    numbers = list(range(1, N * N)) #Generate a list of numbers from 1 to N*N - 1 (excluding 0)
-    random.shuffle(numbers) #Shuffle the list of numbers randomly to create a random puzzle configuration
-    numbers_iter = iter(numbers)    #Create an iterator for the shuffled numbers
-
-    for i in range(N):  #Populate the game board with shuffled numbers, leaving one cell empty (0)
-        for j in range(N):  #If we are at the last cell, set it as an empty cell (0)
-            if (i == N - 1) and (j == N - 1):
-                board[i][j] = 0  # Empty cell
-            else:   #Place the next shuffled number on the board
-                board[i][j] = next(numbers_iter)
-
-    return board #Return the randomly generated game board
-
-def isGoal(board): #Define a function to check if a given board is in a goal state
-    N = len(board)  #Get the size of the board (N x N)
-    n = 1   #Initialize a variable to keep track of the expected value
+    board = [[0] * N for _ in range(N)]
+    numbers = list(range(1, N * N))
+    random.shuffle(numbers)
     for i in range(N):
-        for j in range(N):  #Check if the current cell value is equal to the expected value
-            if board[i][j] != n:
-                return False    #If not, return False
-            n += 1  #Increment the expected value
-            if n == N * N:
-                return True #If all values are in order, return True
+        for j in range(N):
+            if i == N - 1 and j == N - 1:
+                board[i][j] = 0
+            else:
+                board[i][j] = numbers.pop(0)
+    return board
 
-def manhattanDistance(board):  #Define a function to calculate the Manhattan distance heuristic for a given board
-    N = len(board)  # Get the size of the board (N x N)
-    distance = 0    # Initialize the total Manhattan distance
+def isGoal(board):
+    N = len(board)
+    n = 1
+    for i in range(N):
+        for j in range(N):
+            if n == N * N:
+                return board[i][j] == 0
+            if board[i][j] != n:
+                return False
+            n += 1
+
+def manhattanDistance(board):
+    N = len(board)
+    distance = 0
     for i in range(N):
         for j in range(N):
             if board[i][j] == 0:
-                continue    #Skip the empty cell
-            targetVal = board[i][j] - 1    #Calculate the target value for the cell
-            targetX, targetY = divmod(targetVal, N)  #Calculate the target position
-            distance += abs(i - targetX) + abs(j - targetY)   #Calculate Manhattan distance
+                continue
+            target_val = board[i][j] - 1
+            target_x, target_y = divmod(target_val, N)
+            distance += abs(i - target_x) + abs(j - target_y)
     return distance
 
-def idaStar(board):    #Define the IDA* (Iterative Deepening A*) search algorithm
-    N = len(board)  #Get the size of the board (N x N)
-    path = []   #Initialize a list to store the solution path
-    
-    def dfs(board, g, bound, path): #Define a depth-first search function with a depth limit (bound)
-        h = manhattanDistance(board)   #Calculate the Manhattan distance heuristic
-        f = g + h   #Calculate the evaluation function value (f = g + h)
+def idaStar(board):
+    N = len(board)
+    path = []
+    visited = set()
+
+    def dfs(board, g, bound):
+        state_str = str(board)
+        if state_str in visited:
+            return float('inf')
+        visited.add(state_str)
+
+        h = manhattanDistance(board)
+        f = g + h
+
         if f > bound:
-            return f    #If f is greater than the current bound, prune this branch
+            return f
+
         if isGoal(board):
-            return -1   #If the board is in the goal state, return -1 to signal success
-        minBound = float('inf')    #Initialize the minimum bound for this branch
-        
-        zeroX, zeroY = 0, 0   #Initialize the coordinates of the empty cell
+            return -1
+
+        min_bound = float('inf')
+        zero_x, zero_y = 0, 0
+
         for i in range(N):
             for j in range(N):
                 if board[i][j] == 0:
-                    zeroX, zeroY = i, j   #Find the coordinates of the empty cell
-                    
-        for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:   #Try moving the empty cell in four possible directions: right, down, left, up
-            newX, newY = zeroX + dx, zeroY + dy #Calculate the new position
-            if 0 <= newX < N and 0 <= newY < N:   #Swap the values between the empty cell and the neighboring cell
-                board[zeroX][zeroY], board[newX][newY] = board[newX][newY], board[zeroX][zeroY]
-                path.append((newX, newY)) #Add the move to the path
-                
-                t = dfs(board, g + 1, bound, path)  #Recursively explore this move with an increased depth (g + 1)
-                
-                if t == -1:
-                    return -1   #If the goal is found in this branch, return -1
-                if t < minBound:
-                    minBound = t   #Update the minimum bound
-                
-                path.pop()  #Remove the move from the path (backtrack)
-                board[zeroX][zeroY], board[newX][newY] = board[newX][newY], board[zeroX][zeroY] #Undo the move
-        
-        return minBound    #Return the minimum bound for this branch
+                    zero_x, zero_y = i, j
 
-    bound = manhattanDistance(board)   #Initialize the bound with the Manhattan distance heuristic
+        for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            new_x, new_y = zero_x + dx, zero_y + dy
+
+            if 0 <= new_x < N and 0 <= new_y < N:
+                board[zero_x][zero_y], board[new_x][new_y] = board[new_x][new_y], board[zero_x][zero_y]
+                path.append((new_x, new_y))
+
+                t = dfs(board, g + 1, bound)
+
+                if t == -1:
+                    return -1
+                if t < min_bound:
+                    min_bound = t
+
+                path.pop()
+                board[zero_x][zero_y], board[new_x][new_y] = board[new_x][new_y], board[zero_x][zero_y]
+
+        visited.remove(state_str)
+        return min_bound
+
+    bound = manhattanDistance(board)
     while True:
-        t = dfs(board, 0, bound, path)  #Perform iterative deepening search
+        visited.clear()
+        t = dfs(board, 0, bound)
         if t == -1:
-            return path #If a solution is found, return the solution path
+            return path
         if t == float('inf'):
-            return None #If no solution is found, return None
+            return None
+        bound = t
 
 if __name__ == "__main__":
-    N = 4   #Specify the size of the board (N x N)
-    board = generateRandomBoard(N)  #Generate a random starting board
+    N = 4
+    board = generateRandomBoard(N)
 
     print("Random Initial Board:")
     for row in board:
         print(row)
-    
-    solutionPath = idaStar(board)   #Run the IDA* algorithm to find a solution path
-    if solutionPath:    #Display the result
+
+    solutionPath = idaStar(board)
+    if solutionPath:
         print("\nSolution found:")
         for move in solutionPath:
-            print(move) #Print each move in the solution path
+            print(move)
     else:
-        print("\nNo solution found")    #Print a message if no solution is found
+        print("\nNo solution found")
