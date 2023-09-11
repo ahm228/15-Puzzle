fcbe87b10476abf04074b723cad3ab3aea15b6e3
ahm228 <94765616+ahm228@users.noreply.github.com>
2023-09-11 18:12:55 -0500

    Updated README and comments

--- 15puzzle.py
+++ 15puzzle.py
@@ -1,29 +1,29 @@
 import random
 
-# Function to generate a random and solvable NxN puzzle board
+#Function to generate a random and solvable NxN puzzle board
 def generateRandomBoard(boardSize):
-    # Continue trying until a solvable board is found
+    #Continue trying until a solvable board is found
     while True:
-        # Initialize a 1D board of length boardSize * boardSize with zeros
+        #Initialize a 1D board of length boardSize * boardSize with zeros
         board = [0] * (boardSize * boardSize)
         
         # Create a list of numbers from 1 up to (boardSize * boardSize) - 1
         # These numbers represent the tiles that will go on the board
         numbers = list(range(1, boardSize * boardSize))
         
-        # Shuffle the list of numbers to randomize the initial state of the board
+        #Randomize the initial state of the board
         random.shuffle(numbers)
         
-        # Fill in the board with the shuffled numbers
+        #Fill in the board with the shuffled numbers
         for i in range(boardSize * boardSize - 1):
             board[i] = numbers.pop()
         
-        # Set the last position on the board as empty (0)
+        #Set the last position on the board as 0
         board[-1] = 0
 
-        # Check if the generated board is solvable
+        #Check if the generated board is solvable
         if isSolvable(board, boardSize):
-            # If it is solvable, return the board
+            #If it is, return the board
             return board
         
 def printBoard(board, boardSize):
@@ -32,74 +32,70 @@ def printBoard(board, boardSize):
             print(board[i * boardSize + j], end='\t')
         print()
 
-# Function to check if the current board is in the goal state.
+#Function to check if the current board is in the goal state
 def isGoal(board, boardSize):
-    # Check if the last tile is 0 (the empty tile should be in the final position)
+    #Check if the last tile is 0
     if board[-1] != 0:
         return False
     
-    # Loop through the rest of the board
+    #Loop through the rest of the board
     for i in range(boardSize * boardSize - 1):
-        # Each tile should be at its correct position (i.e., tile numbered 'n' should be at index 'n - 1')
+        #Each tile should be at its correct position (tile n should be at index n - 1)
         if board[i] != i + 1:
             return False
-    
-    # If the function hasn't returned False yet, the board is in the goal state
+        
     return True
 
-# Function to calculate the Manhattan distance heuristic for a given board
+#Function to calculate the Manhattan distance heuristic for a given board
 def manhattanDistance(board, boardSize):
-    # Initialize the variable that will store the total Manhattan distance for the board
     distance = 0
     
-    # Loop through all positions in the flattened 1D board
+    #Loop through all positions in the flattened 1D board
     for i in range(boardSize * boardSize):
-        # Skip if the tile is empty (value is 0)
+        #Skip if the tile is 0
         if board[i] == 0:
             continue
         
-        # Calculate the target position (goal state) of the current tile
-        targetVal = board[i] - 1  # Target value is one less than the current value
+        #Calculate the target position (goal state) of the current tile
+        targetVal = board[i] - 1  #Target value is one less than the current value
         
-        # Convert 1D index to 2D coordinates for current position
-        x, y = divmod(i, boardSize)  # x and y are row and column coordinates, respectively
+        #Convert 1D index to 2D coordinates for current position
+        x, y = divmod(i, boardSize)
         
-        # Convert 1D index to 2D coordinates for target position
-        targetX, targetY = divmod(targetVal, boardSize)  # targetX and targetY are row and column coordinates, respectively
+        #Convert 1D index to 2D coordinates for target position
+        targetX, targetY = divmod(targetVal, boardSize)
         
-        # Add the Manhattan distance for the current tile to the total distance
-        # Manhattan distance is calculated as the sum of the horizontal and vertical distances to the target position
+        #Add the Manhattan distance for the current tile to the total distance
+        #Manhattan distance is calculated as the sum of the horizontal and vertical distances to the target position
         distance += abs(x - targetX) + abs(y - targetY)
     
-    # Return the total Manhattan distance for the board
     return distance
 
-# Function to incrementally update Manhattan distance after making a move
+#Function to incrementally update Manhattan distance after making a move
 def manhattanDelta(board, oldIndex, newIndex, boardSize):
-    # Initialize the delta variable, which will store the change in Manhattan distance
+    #Initialize the delta variable, which will store the change in Manhattan distance
     delta = 0
     
-    # Loop through both old and new positions of the moved tile
+    #Loop through both old and new positions of the moved tile
     for oi, ni in [(oldIndex, newIndex), (newIndex, oldIndex)]:
-        # Retrieve the value at the old position
+        #Retrieve the value at the old position
         value = board[oi]
         
-        # Skip if the tile is empty (value is 0)
+        #Skip if the tile is 0
         if value == 0:
             continue
         
-        # Calculate the target position of the tile
+        #Calculate the target position of the tile
         targetVal = value - 1
-        x, y = divmod(oi, boardSize)  # Current x and y coordinates
-        targetX, targetY = divmod(targetVal, boardSize)  # Target x and y coordinates
-        newX, newY = divmod(ni, boardSize)  # New x and y coordinates
+        x, y = divmod(oi, boardSize)
+        targetX, targetY = divmod(targetVal, boardSize)
+        newX, newY = divmod(ni, boardSize)
         
-        # Update the delta (change in Manhattan distance)
-        # Add the Manhattan distance from the new position to the target position
-        # Subtract the Manhattan distance from the old position to the target position
+        #Update the change in Manhattan distance
+        #Add the Manhattan distance from the new position to the target position
+        #Subtract the Manhattan distance from the old position to the target position
         delta += abs(targetX - newX) + abs(targetY - newY) - abs(targetX - x) - abs(targetY - y)
     
-    # Return the calculated delta value
     return delta
 
 '''
@@ -111,25 +107,25 @@ because each swap changes the parity (odd to even or even to odd), making it imp
 state with an even number of inversions.
 '''
 
-# Function to check if a given board is solvable
+#Function to check if a given board is solvable
 def isSolvable(board, boardSize):
-    # Initialize the inversion counter to 0
+    #Initialize the inversion counter to 0
     inversionCount = 0
     
-    # Loop through the board to count inversions
-    for i in range(len(board) - 1):  # Iterate from the first element to the second-to-last element
-        if board[i] == 0:  # Skip the zero tile
+    #Loop through the board to count inversions
+    for i in range(len(board) - 1):  #Iterate from the first element to the penultimate element
+        if board[i] == 0:  #Skip the zero tile
             continue
        
-        for j in range(i + 1, len(board)):  # Iterate from the element after i to the last element
-            if board[j] == 0:  # Skip the zero tile
+        for j in range(i + 1, len(board)):  #Iterate from the element after i to the last element
+            if board[j] == 0:  #Skip the zero tile
                 continue
             
-            # Check if an inversion exists, i.e., a larger number appears before a smaller number
+            #Check if an inversion exists
             if board[i] > board[j]:
-                inversionCount += 1  # Increment the inversion count
+                inversionCount += 1
     
-    # For a board to be solvable, the number of inversions must be even
+    #For a board to be solvable, the number of inversions must be even
     return inversionCount % 2 == 0
 
 #Function to perform the IDA* search algorithm to find a solution path
@@ -149,15 +145,15 @@ def idaStar(board, boardSize):
         
         minBound = float('inf') #Initialize minBound to an arbitrarily large number
         
-        for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]: #Explore adjacent moves (up, down, left, right)
-            x, y = divmod(zeroIndex, boardSize) #Get the current x, y coordinates of the zero (empty cell)
-            newX, newY = x + dx, y + dy #Calculate new coordinates for zero after mov
+        for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]: #Explore adjacent moves
+            x, y = divmod(zeroIndex, boardSize) #Get the current x, y coordinates of the zero
+            newX, newY = x + dx, y + dy
             
             if 0 <= newX < boardSize and 0 <= newY < boardSize: #Check if new coordinates are valid (within bounds)
-                newZeroIndex = newX * boardSize + newY #Calculate new linear index for zero
+                newZeroIndex = newX * boardSize + newY #Calculate new index for zero
                 delta = manhattanDelta(board, zeroIndex, newZeroIndex, boardSize) #Calculate how this move will affect the Manhattan distance
                 h += delta #Add the delta to the heuristic
-                board[zeroIndex], board[newZeroIndex] = board[newZeroIndex], board[zeroIndex] #Perform the move (swap zero and the adjacent cell)
+                board[zeroIndex], board[newZeroIndex] = board[newZeroIndex], board[zeroIndex] #Perform the move
                 path.append(newZeroIndex) #Add this move to the path
                 t = dfs(board, g + 1, bound, newZeroIndex) #Recursively call DFS with new board state
                 
@@ -166,16 +162,18 @@ def idaStar(board, boardSize):
                 
                 if t < minBound: #Update the minimum bound needed for next iteration
                     minBound = t
-                path.pop() #Backtrack, undo the move and remove the last added move in path
+
+                #Backtrack, undo the move and remove the last added move in path
+                path.pop() 
                 board[zeroIndex], board[newZeroIndex] = board[newZeroIndex], board[zeroIndex]
                 h -= delta #Subtract the delta from the heuristic to backtrack
 
-        return minBound #Return the minimum bound for the next iteration
+        return minBound
 
     bound = manhattanDistance(board, boardSize)
     zeroIndex = board.index(0)
 
-    #Enter an infinite loop, will break when solution is found or proven unsolvable
+    #Enter an infinite loop, will break when solution is found
     while True:
         t = dfs(board, 0, bound, zeroIndex) #Call the DFS function to explore nodes up to the current bound
         
@@ -188,20 +186,15 @@ def idaStar(board, boardSize):
         bound = t #Update the bound for the next round of IDA*
 
 if __name__ == "__main__":
-    boardSize = 4 #consider taking this as an input, allow for board sizes other than 4 x 4
+    boardSize = 4 #NOTE: consider taking this as an input, allowing for board sizes other than 4 x 4
     board = generateRandomBoard(boardSize)
 
     print("Random Initial Board:")
     printBoard(board, boardSize)
     
-    if isSolvable(board, boardSize):
-        solutionPath = idaStar(board, boardSize)
+    solutionPath = idaStar(board, boardSize)
         
-        if solutionPath:
-            print("\nSolution found:", solutionPath)
-        
-        else:
-            print("\nNo solution found")
-
+    if solutionPath:
+        print("\nSolution found:", solutionPath)
     else:
-        print("\nThis puzzle is not solvable")
+        print("\nNo solution found")
--- README.md
+++ README.md
@@ -1,50 +1,41 @@
 N-Puzzle Solver using IDA* Algorithm
 
-    This Python script solves the N-Puzzle problem using the IDA* (Iterative-Deepening A*) algorithm. It employs Manhattan distance as a heuristic to estimate the cost of reaching the goal state from the current state. The code is capable of generating random, solvable N x N boards and finding the solution path.
-    
+    This Python script solves the N-Puzzle problem using the IDA* (Iterative-Deepening A*) algorithm. The program employs a 1D list to represent the board state and utilizes an efficient method to update the Manhattan distance after each move. It is capable of generating random, solvable N x N boards and finding the solution path.
+
     Functions
+
         generateRandomBoard(boardSize)
             This function generates a random solvable N x N puzzle board. It uses a while loop to continue generating boards until it finds one that is solvable.
-        
-        isGoal(board)
-            This function checks if the current board state is the goal state by iterating through the board's elements and comparing them with the expected goal state.
 
-        manhattanDistance(board)
+        isGoal(board, boardSize)
+            This function checks if the current board state is the goal state. It ensures that the last tile is zero and that the rest of the tiles are in ascending order.
+
+        manhattanDistance(board, boardSize)
             This function calculates the Manhattan distance for a given board state. Manhattan distance is the sum of the absolute differences between each tile's current position and its position in the goal state.
 
-        manhattanDelta(board, oldX, oldY, newX, newY, boardSize)
-            This function incrementally updates the Manhattan distance after each move to improve the performance of the heuristic calculation.
+        manhattanDelta(board, oldIndex, newIndex, boardSize)
+            This function incrementally updates the Manhattan distance after each move. This improves the efficiency of heuristic calculation.
 
-        isSolvable(board)
+        isSolvable(board, boardSize)
             This function checks if a given board is solvable by counting the number of inversions in the board. An inversion is a pair of tiles that are in the wrong order relative to their positions in the goal state.
 
-        idaStar(board)
-            This function performs the IDA* search algorithm to find the solution path for a given board. It explores different states of the board by moving the empty tile (0) and updates the cost (f) of each state to determine the most promising path to the goal state.
+        idaStar(board, boardSize)
+            This function performs the IDA* search algorithm to find the solution path for a given board. It explores different states of the board by swapping the empty tile (0) with adjacent tiles and updates the bound based on the heuristic and cost-to-come.
 
     Usage
 
         Run the script.
         The initial random board will be displayed.
-        The script will output each board state as it iterates through the IDA* algorithm.
+        The script will output the current board state at each step as it iterates through the IDA* algorithm.
         If a solution is found, the solution path will be displayed.
 
     Example Output
+    
+    Random Initial Board:
+    0	12	5	8	
+    7	15	1	3	
+    2	10	11	9	
+    4	13	14	6	
 
-        Random Initial Board:
-        [12, 5, 8, 3]
-        [7, 15, 1, 9]
-        [2, 10, 11, 14]
-        [4, 13, 6, 0]
-
-        Solution found:
-        [(3, 2), (2, 2), (1, 2), ...]
-
-    or
-
-        Random Initial Board:
-        [7, 2, 3, 8]
-        [9, 5, 1, 4]
-        [0, 15, 14, 10]
-        [13, 11, 12, 6]
-
-        This puzzle is not solvable
+    Solution found:
+    [(3, 2), (2, 2), (1, 2), ...]
