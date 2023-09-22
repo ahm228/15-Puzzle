07ca4b40a7de280290d7a19d6ad89803f1db868f
ahm228 <94765616+ahm228@users.noreply.github.com>
2023-09-21 21:28:44 -0500

    Updated commentation

--- 15puzzle.py
+++ 15puzzle.py
@@ -3,20 +3,16 @@ import random
 def generateRandomBoard(boardSize, numMoves=70):
     #Create a 1D list representing the board, initialize it with numbers from 1 to (boardSize*boardSize - 1) and append a 0 at the end
     board = list(range(1, boardSize * boardSize)) + [0]
-    #Initialize the position of the zero (empty slot) on the board
-    zeroIndex = boardSize * boardSize - 1
+    zeroIndex = boardSize * boardSize - 1 #Initialize the position of the zero (empty slot) on the board
 
     #Perform numMoves number of random moves to shuffle the board
     for _ in range(numMoves):
-        #Initialize an empty list to store the possible new positions for the zero tile
-        possibleMoves = []
+        possibleMoves = [] #Initialize an empty list to store the possible new positions for the zero tile
         
         #Loop through possible moves
         for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-            #Calculate the current x, y coordinate of the zero based on its index in the 1D list
-            x, y = divmod(zeroIndex, boardSize)
-            #Calculate the new potential x, y coordinates for the zero
-            newX, newY = x + dx, y + dy
+            x, y = divmod(zeroIndex, boardSize) #Calculate the current x, y coordinate of the zero based on its index in the 1D list
+            newX, newY = x + dx, y + dy #Calculate the new potential x, y coordinates for the zero
             
             #Check if the new coordinates are within the bounds of the board
             if 0 <= newX < boardSize and 0 <= newY < boardSize:
@@ -25,6 +21,7 @@ def generateRandomBoard(boardSize, numMoves=70):
 
         #Randomly select one of the possible moves        
         newZeroIndex = random.choice(possibleMoves)
+        
         #Swap the zero and the selected tile
         board[zeroIndex], board[newZeroIndex] = board[newZeroIndex], board[zeroIndex]
         zeroIndex = newZeroIndex
@@ -80,58 +77,89 @@ def manhattanDistance(board, boardSize):
     
     return distance
 
-# Function to incrementally update Manhattan distance after making a move
+#Function to incrementally update Manhattan distance after making a move
 def manhattanDelta(board, oldIndex, newIndex, boardSize):
-    delta = 0
+    delta = 0 #Initialize a variable delta to store the change in Manhattan distance
+
+    #Loop through both old and new positions of the moved tile
+    #Use this loop to compute the delta for both the tile that's moving to the zero tile
+    #and the zero tile itself that's moving to the tile's spot
     for oi, ni in [(oldIndex, newIndex), (newIndex, oldIndex)]:
-        value = board[oi]
-        if value == 0:
+        value = board[oi] #Retrieve the value at the old position
+        
+        if value == 0: #Skip if the tile is the zero tile
             continue
-        targetVal = value - 1
+
+        #Calculate the target position of the tile based on its ideal position in the solved board
+        targetVal = value - 1 #Tile n should be at index n-1 in the solved board
         x, y = divmod(oi, boardSize)
         targetX, targetY = divmod(targetVal, boardSize)
         newX, newY = divmod(ni, boardSize)
+
+        #Update the change in Manhattan distance
+        #Add the Manhattan distance of the tile at its new position to the target position
+        #and subtract the Manhattan distance of the tile at its old position to the target position
         delta += abs(targetX - newX) + abs(targetY - newY) - abs(targetX - x) - abs(targetY - y)
+
     return delta
 
-# Updated DFS function
-def dfs(board, g, bound, zeroIndex, h, path):  # Add 'path' as an argument
-    f = g + h
+
+def dfs(board, g, bound, zeroIndex, h, path):
+    #Calculate the total estimated cost 'f' for the current board state
+    f = g + h #g = actual cost, h = heuristic cost (Manhattan distance)
+
+    #If the estimated cost exceeds the current bound, return the estimated cost
     if f > bound:
         return f
+    
     if isGoal(board, boardSize):
         return -1
-    minBound = float('inf')
+    
+    minBound = float('inf') #Initialize minBound to an arbitrarily large number
+
     for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
         x, y = divmod(zeroIndex, boardSize)
         newX, newY = x + dx, y + dy
+
         if 0 <= newX < boardSize and 0 <= newY < boardSize:
             newZeroIndex = newX * boardSize + newY
             delta = manhattanDelta(board, zeroIndex, newZeroIndex, boardSize)
-            h += delta
+            h += delta #Update the heuristic cost based on the move
+
+            #Perform the move by swapping the zero tile and the adjacent tile
             board[zeroIndex], board[newZeroIndex] = board[newZeroIndex], board[zeroIndex]
             path.append(newZeroIndex)
-            t = dfs(board, g + 1, bound, newZeroIndex, h, path)  # Pass 'path' to the recursive call
-            if t == -1:
+
+            #Recursively call DFS with the new board state
+            t = dfs(board, g + 1, bound, newZeroIndex, h, path)
+
+            if t == -1: #If a solution is found, return -1 to indicate success
                 return -1
-            if t < minBound:
+            
+            if t < minBound: #Update 'minBound' if the estimated cost 't' is lower
                 minBound = t
+
+            #Backtrack
             path.pop()
             board[zeroIndex], board[newZeroIndex] = board[newZeroIndex], board[zeroIndex]
-            h -= delta  # Revert the heuristic to its previous value
+            h -= delta
+
     return minBound
 
-# Updated IDA* function
 def idaStar(board, boardSize):
-    path = []
-    bound = manhattanDistance(board, boardSize)
-    h = bound  # Initialize h to the initial full Manhattan distance
-    zeroIndex = board.index(0)
+    path = [] #Initialize an empty list to keep track of the solution path
+    bound = manhattanDistance(board, boardSize) #Calculate the initial heuristic cost (Manhattan distance) for the board
+    h = bound #Initialize h to the initial full Manhattan distance
+    zeroIndex = board.index(0) #Find the index of the zero tile in the board
+
     while True:
-        t = dfs(board, 0, bound, zeroIndex, h, path)  # Pass 'path' as an argument
-        if t == -1:
+        #Perform depth-first search up to the current bound
+        t = dfs(board, 0, bound, zeroIndex, h, path)
+        
+        if t == -1: #If a solution is found, break the loop and return the path
             return path
-        bound = t
+        
+        bound = t #Update the bound for the next iteration
 
 
 if __name__ == "__main__":
